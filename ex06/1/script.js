c432fe7e39297e5f8dd4a88419d256678b5cce89
Guilherme Mendes Rosa <guilhermemr.jlle@gmail.com>
2023-09-21 11:38:03 -0300

    Update script.js

--- ex06/1/script.js
+++ ex06/1/script.js
@@ -3,16 +3,16 @@ const campo2 = document.getElementById('campo2');
 const campo3 = document.getElementById('campo3');
 const radios = document.getElementsByName('conversao');
 
-for (let i = 0; i < radios.length; i++) {
-    radios[i].addEventListener('change', function () {
-        if (radios[i].value === 'maiusculas') {
+radios.forEach(radio => {
+    radio.addEventListener('change', function () {
+        if (radio.value === 'maiusculas') {
             campo1.value = campo1.value.toUpperCase();
             campo2.value = campo2.value.toUpperCase();
             campo3.value = campo3.value.toUpperCase();
-        } else if (radios[i].value === 'minusculas') {
+        } else if (radio.value === 'minusculas') {
             campo1.value = campo1.value.toLowerCase();
             campo2.value = campo2.value.toLowerCase();
             campo3.value = campo3.value.toLowerCase();
         }
-    });
-}
+    })
+});
