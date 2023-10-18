0dd862faa2190cb9ec71688b93771d61ae57da2e
Guilherme Mendes Rosa <guilhermemr.jlle@gmail.com>
2023-10-18 20:06:07 -0300

    .

---
+++ ex07/script.js
@@ -0,0 +1,34 @@
+
+function processExercise() {
+    debugger
+    const exercicio = document.getElementById('exercicio').value;
+    const series = document.getElementById('series').value;
+
+    if (!exercicio || !series) {
+        event.preventDefault();
+        alert('Por favor, preencha todos os campos!');
+        return
+    }
+
+    let seriesValue = parseInt(document.getElementById('series').value);
+
+    const totalSeries = parseInt(localStorage.getItem('totalSeries'));
+    if (totalSeries >= 0) {
+        seriesValue += totalSeries;
+    }
+
+    localStorage.setItem('totalSeries', seriesValue);
+
+    let exercises = localStorage.getItem('exercises');
+    if (!exercises) {
+        exercises = [];
+    } else {
+        exercises = [...localStorage.getItem('exercises')]; 
+    }
+    exercises.push(exercicio)
+    localStorage.setItem('exercises', exercises);
+
+
+    alert('Total de Series: ' + seriesValue);
+    alert('Exercícios: ' + exercises);
+}
